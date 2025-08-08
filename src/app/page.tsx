441658bc1f4cad5bfe13f0fafca48f188ed7f4d4
MatheusZamo <19matheushenrique03@gmail.com>
2025-08-07 23:40:39 -0300

    feat: add category selector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 
+import CategorySelector from "@/components/common/category-selector";
 import Header from "@/components/common/header";
 import ProductList from "@/components/common/product-list";
 import { db } from "@/db";
@@ -10,6 +11,8 @@ const Home = async () => {
       variants: true,
     },
   });
+
+  const categories = await db.query.categoryTable.findMany({});
 
   return (
     <>
@@ -24,6 +27,11 @@ const Home = async () => {
           className="h-auto w-full"
         />
         <ProductList products={products} title={"Mais vendidos"} />
+
+        <div className="px-5">
+          <CategorySelector categories={categories} />
+        </div>
+
         <Image
           src="/banner-02.svg"
           alt="Leve uma vida com estilo"
